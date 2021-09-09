ec4575bdb45454383a991f082c4102b8be9bb8d2
abner-ribeiro <abner.ribeiro@zup.com.br>
2021-09-09 17:01:39 -0300

    piloto: Abner Filipe

---
+++ src/test/java/br/com/zup/edu/pizzaria/pizzas/PizzaTest.java
@@ -0,0 +1,7 @@
+package br.com.zup.edu.pizzaria.pizzas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PizzaTest {
+
+}
--- src/test/java/br/com/zup/edu/pizzaria/pizzas/cadastropizza/NovaPizzaControllerTest.java
+++ src/test/java/br/com/zup/edu/pizzaria/pizzas/cadastropizza/NovaPizzaControllerTest.java
@@ -54,4 +54,61 @@ class NovaPizzaControllerTest {
 
     }
 
+    @Test
+    void naoDeveCadatrarSemIngrediente() throws Exception {
+        List<Long> ingredientes = new ArrayList<>();
+
+        NovaPizzaRequest body = new NovaPizzaRequest("Peperone", ingredientes);
+        MockHttpServletRequestBuilder request = post("/api/pizzas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(body));
+
+        mvc.perform(request)
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void naoDeveCadastrarPizzaComSaborRepetido() throws Exception {
+        ingredienteRepository.save(new Ingrediente("Peperone", 1, new BigDecimal(10.0)));
+        ingredienteRepository.save(new Ingrediente("Tomate", 1, new BigDecimal(10.0)));
+
+        List<Long> ingredientes = new ArrayList<>();
+        ingredientes.add(1L);
+        ingredientes.add(2L);
+
+        NovaPizzaRequest body = new NovaPizzaRequest("Peperone", ingredientes);
+        MockHttpServletRequestBuilder request = post("/api/pizzas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(body));
+
+
+        mvc.perform(request)
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andExpect(redirectedUrlPattern("/api/pizzas/*"));
+
+        mvc.perform(request)
+                .andExpect(status().isBadRequest());
+
+    }
+
+    @Test
+    void naoDeveCadastrarPizzaComSaborNulo() throws Exception {
+        ingredienteRepository.save(new Ingrediente("Peperone", 1, new BigDecimal(10.0)));
+        ingredienteRepository.save(new Ingrediente("Tomate", 1, new BigDecimal(10.0)));
+
+        List<Long> ingredientes = new ArrayList<>();
+        ingredientes.add(1L);
+        ingredientes.add(2L);
+
+        NovaPizzaRequest body = new NovaPizzaRequest(null, ingredientes);
+        MockHttpServletRequestBuilder request = post("/api/pizzas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(body));
+
+        mvc.perform(request)
+                .andExpect(status().isBadRequest());
+
+    }
+
 }
